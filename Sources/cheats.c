f8d9b0237a031c2b1084474281effe9160722d82
AnalogMan151 <james.p.wolfe@outlook.com>
2017-07-13 15:34:47 -0400

    Added example cheat entries to menu
    including toggle, auto disable and with notes or without notes

---
+++ Sources/cheats.c
@@ -0,0 +1,16 @@
+#include "cheats.h"
+#include "hid.h"
+
+void    nullCheat(void)
+{
+    // Code that runs continiously while cheat is on
+}
+
+void    nullCheatToggle(u32 state)
+{
+    if (state) {
+        // Code to run continiously when cheat is on
+    } else {
+        // Code to run once when cheat is disabled
+    }
+}
--- Sources/menu.c
+++ Sources/menu.c
@@ -5,7 +5,7 @@
 
 char	*builder_name = "AnalogMan",
         version[7] = "v1.0.0",
-        formattedVer[22];
+        formattedVer[27];
 
 
 
@@ -16,10 +16,17 @@ void    always_run(void) {
 
 void	my_menus(void) {
     overlayInit();
-    xsprintf(formattedVer, "%14s", version);
+    xsprintf(formattedVer, "%19s", version);
     new_unselectable_entry("Entries w/ an orange background");
     new_unselectable_entry("have notes. Press (Y) to view.");
     new_super_unselectable_entry(formattedVer, always_run);
 	new_separator();
+
+    new_entry_managed_note("Name of cheat", "Cheat note", nullCheat, NULLCHEAT, 0);
+    new_entry_managed("Name of cheat", nullCheat, NULLCHEAT, 0);
+    new_entry_managed("Name of Cheat", nullCheat, NULLCHEAT, AUTO_DISABLE);
+
+    new_entry_arg_note("Name of cheat", "Cheat note", nullCheatToggle, 0, NULLCHEATTOGGLE, TOGGLE);
+    new_entry_arg("Name of cheat", nullCheatToggle, 0, NULLCHEATTOGGLE, TOGGLE);
 
 }
